e1ba1036eac6444549d442341647550b3cd80a84
pfm <muenzer.philipp@outlook.com>
2025-05-23 23:30:46 +0100

    Changed read_txt and added fill_path

--- PYTHON/process.py
+++ PYTHON/process.py
@@ -9,20 +9,22 @@ import os
 import pickle
 import numpy as np
 
+#__________________________________________________________________________________
 def read_txt(filename, path='', deler=',', skro=0, header=False, kwargs = dict()):
     '''
     Extracts data from a file with arbitrary deliminiter using numpy.
     
     :param filename: Filename to be opened. Can be whole path or just the name.
-    :param path: Path to save the object in. If empty, attempt to split up the filename. Fallback is current working directory.
+    :param path: Path to save the object in. If empty, attempt to split up 
+    the filename. Fallback is current working directory.
     :param deler: Deliminiter to be used. Default is ','.
     :param skro: Number of rows to skip. Default is 0.
     :param header: Whether header is to be extracted. Default is False
     :param kwargs: Additional arguments to be passed to numpy.loadtxt.
     :return: Data extracted from the file and the header as a tuple. 
     '''
-
-    
+    # Fill path if needed
+    path, filename = fill_path(path,filename)
 
     # Check extract header
     if header:
@@ -34,14 +36,7 @@ def read_txt(filename, path='', deler=',', skro=0, header=False, kwargs = dict()
         # Now extract the header
         header = np.loadtxt()
 
-        
-    
-    
-
-
-
-
-
+#__________________________________________________________________________________
 def data_extractor(filename, path='', mode='auto'):
     '''
     Extracts data from a file. The file is opened in the given mode.
@@ -54,6 +49,7 @@ def data_extractor(filename, path='', mode='auto'):
     '''
 
 
+#__________________________________________________________________________________
 def seperate_file_folder(dir):
     '''
     Splits a path into file and directory. If the dir is empty, exception is raised.
@@ -68,6 +64,31 @@ def seperate_file_folder(dir):
     # split the path abd return the file and directory
     return os.path.split(dir)
 
+#__________________________________________________________________________________
+def fill_path(path, filename, fallback='cwd'):
+    '''
+    Checks wether path is empty, if yes attempting to split filename. If not succesful, fallback is used.
+    
+    :param filename: Filename for possible splitting.
+    :param path: Path to be checked.
+    :param fallback: Fallback to be used if path is empty. Default is 'cwd'.
+    :return: Tuple of path and filename.
+    '''
+    # Check wether path is empty == if yes, try to split up the filename
+    if path == '':
+        path, filename = seperate_file_folder(filename)
+    
+        # Fallback is current path
+        if path == '':
+            match fallback: 
+                case 'cwd':
+                    path = os.getcwd()
+                case _: 
+                    raise ValueError('Fallback unknown!')
+    
+    return path, filename
+
+#__________________________________________________________________________________
 def save_Object(obj, filename, path='', format='.pkl'):
     '''
     Saves an Object in the path. Any object can be saved. Target format is pickle.
@@ -78,13 +99,10 @@ def save_Object(obj, filename, path='', format='.pkl'):
     :param format: Format to save the object in. Default is ".pkl". 
     :return: None
     '''
-    #Check wether path is empty == if yes, try to split up the filename
-    if path == '':
-        path, filename = seperate_file_folder(filename)
-
-    if path == '':
-        path = os.getcwd()
     
+    # Filling path if necessary
+    path, filename = fill_path(path, filename)
+
     # Check wether path exists and if not, create it!
     if not os.path.exists(path):
         os.makedirs(path)
@@ -99,6 +117,6 @@ def save_Object(obj, filename, path='', format='.pkl'):
         raise ValueError('Format not supported!')
     print('Done!')
 
-
+#__________________________________________________________________________________
 if __name__ == "__main__":
     print("Module for input and output of data, e.g. reading files.")
